914aaf6c16d1c9afb10a96f766f7c115cc557d8f
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-03-16 13:35:49 +0100

    subclases

---
+++ Electrico.php
@@ -0,0 +1,19 @@
+<?php
+
+class Electrico extends Pokemon
+{
+    public function __construct ($nombre, $tipo, $ataque){
+        parent::__construct($nombre, $tipo, $ataque);
+        $this->elemento = "electrico";
+        $this->nivel = 1;
+        $this->vida = rand(70,160);
+        $this->vida_actual = $this->vida;
+        $this->vida_ataque= rand(150, 300);
+    }
+
+    public function Atacar($pokemon) : string {
+        $pokemon->vida_actual -= $this->vida_ataque*0.6;
+        return $this->getNombre(). " ha atacado a ". $pokemon->getNombre(). " haciendo ". $this->vida_ataque. " de daño";
+    }
+
+}
