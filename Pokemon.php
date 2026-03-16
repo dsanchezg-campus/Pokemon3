1409b6cc5b4ae9b5a9c668ae282c1314cf50ae56
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-03-16 12:47:06 +0100

    Add files via upload

---
+++ Pokemon.php
@@ -0,0 +1,57 @@
+<?php
+class Pokemon {
+    private $nombre;
+    private $elemento;
+    private $tipo;
+    private $ataque;
+    private $nivel;
+    private $vida;
+    private $vida_ataque;
+
+    public function __construct($nombre, $elemento, $tipo, $ataque){
+        $this->nombre = $nombre;
+        $this->elemento = $elemento;
+        $this->tipo = $tipo;
+        $this->ataque = $ataque;
+        $this->nivel = rand(1,100);
+        $this->vida = rand(1,255);
+        $this->vida_ataque=rand(1,255);
+    }
+
+    public function restaVida($danio){
+        $this->vida=$this->vida - $danio;
+        return $this->vida;
+    }
+    public function getNombre() {
+    return $this->nombre;
+}
+
+    public function getDanio(){
+        return $this->vida_ataque;
+    }
+    public function Atacar(){
+        return $this->nombre . " ataca con " . $this->ataque . " y causa daño.";
+    }
+    
+
+    public function Evolucionar(){
+            $this->vida = (($this->vida+ rand(0,31)) * 2 + rand(0,252)/4)/100 + $this->nivel + 10;
+            return $this->vida;
+    }
+
+    public function getVida(){
+        return $this->vida;
+    }
+
+    public function MostrarInfo(){
+    return 
+        "<p>Nombre: " . $this->nombre . "</p>" .
+        "<p>Elemento: " . $this->elemento . "</p>" .
+        "<p>Tipo: " . $this->tipo . "</p>" .
+        "<p>Ataque: " . $this->ataque . "</p>" .
+        "<p>Nivel: " . $this->nivel . "</p>" .
+        "<p>Vida: " . $this->vida . "</p>";
+    }
+
+}
+?>
